16a96489409a491e518f1437d45c796a27dea2dc
Vickthor3T <victor.cardoza@tripleten-team.com>
2025-08-21 07:54:33 -0600

    changes in the index.html and script.js

--- exercise4_js/scripts/script.js
+++ exercise4_js/scripts/script.js
@@ -1,5 +1,3 @@
-//console.log("Hola Mundo");
-
 const cards = [
   {
     image:
@@ -21,58 +19,30 @@ const cards = [
 const travelerProfileAddPlaceBtn = document.querySelector(
   ".traveler-profile__add-place-btn"
 );
-
+const modalProfile = document.querySelector("#modal-profile");
+const ModalNewPlace = document.querySelector("#modal-new-place");
+const modalImageView = document.querySelector("#modal-image-view");
+const travelerProfileDetails = document.querySelector(
+  ".traveler-profile__details"
+);
+const travelerProfileName = document.querySelector(".traveler-profile__name");
+const travelerProfileBio = document.querySelector(".traveler-profile__bio");
+const profileName = document.querySelector("#profile-name");
+const profileDescription = document.querySelector("#profile-description");
+const placesGalleryList = document.querySelector(".places-gallery__list");
+//modalForms here is where we save all the forms in the index.html
+const modalForms = Array.from(document.querySelectorAll(".modal__form"));
+const modalsClose = Array.from(document.querySelectorAll(".modal__close"));
 const travelerProfileEditBtn = document.querySelector(
   ".traveler-profile__edit-btn"
 );
 
-const modalProfile = document.querySelector("#modal-profile");
-const ModalNewPlace = document.querySelector("#modal-new-place");
-const modalImageView = document.querySelector("#modal-image-view");
-
-const travelerProfileDetails = document.querySelector(
-  ".traveler-profile__details"
-);
-const travelerProfileName = travelerProfileDetails.querySelector(
-  ".traveler-profile__name"
-);
-const placesGalleryList = document.querySelector(".places-gallery__list");
-
-/*
-function addCard(){
-    MEMOIZADA
-}*/
-
-const modalsClose = Array.from(document.querySelectorAll(".modal__close"));
-
-travelerProfileEditBtn.addEventListener("click", () => {
-  console.log("Click en el lapizito");
-  modalProfile.classList.toggle("modal_is-opened");
-});
-
-modalsClose.forEach((modalClose) => {
-  modalClose.addEventListener("click", (evt) => {
-    const modal = evt.target.closest(".modal");
-    modal.classList.toggle("modal_is-opened");
+//this function take all the inputs in a form an then validates the data using the required fields
+const validarBoton = (modalInputs) => {
+  return modalInputs.some((inputElement) => {
+    return !inputElement.validity.valid;
   });
-});
-
-travelerProfileAddPlaceBtn.addEventListener("click", () => {
-  //  ModalNewPlace.classList.add("modal_is-opened");
-  ModalNewPlace.classList.toggle("modal_is-opened");
-});
-
-ModalNewPlace.addEventListener("submit", (evt) => {
-  const tempCard = {};
-  evt.preventDefault();
-  modalForm = ModalNewPlace.querySelector(".modal__form");
-  const modalInputs = Array.from(modalForm.querySelectorAll(".modal__input"));
-  modalInputs.forEach((modalInput) => {
-    tempCard[modalInput.name] = modalInput.value;
-  });
-
-  createCard(tempCard);
-});
+};
 
 const createCard = (card) => {
   const templatePlaceCard = document
@@ -88,10 +58,71 @@ const createCard = (card) => {
     ".place-card__delete-button"
   );
 
+  modalProfile.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    travelerProfileName.textContent = profileName.value;
+    travelerProfileBio.textContent = profileDescription.value;
+    modalProfile.classList.toggle("modal_is-opened");
+  });
+
+  modalForms.forEach((modalForm) => {
+    const modalInputs = Array.from(modalForm.querySelectorAll(".modal__input"));
+    const modalButton = modalForm.querySelector(".modal__button");
+    modalButton.disabled = false;
+    modalButton.disabled = validarBoton(modalInputs);
+
+    modalInputs.forEach((modalInput) => {
+      modalInput.addEventListener("input", () => {
+        modalButton.disabled = validarBoton(modalInputs);
+
+        let modalError = modalForm.querySelector(
+          "#" + modalInput.id + "-error"
+        );
+        console.log();
+        if (!modalInput.validity.valid) {
+          modalError.textContent = "Hay Un Error|";
+          modalError.classList.add("modal__error_visible");
+        } else {
+          modalError.textContent = "";
+          modalError.classList.remove("modal__error_visible");
+        }
+      });
+    });
+  });
+
+  travelerProfileEditBtn.addEventListener("click", () => {
+    console.log("Click en el lapizito");
+    profileName.value = travelerProfileName.textContent;
+    profileDescription.value = travelerProfileBio.textContent;
+    modalProfile.classList.toggle("modal_is-opened");
+  });
+
+  modalsClose.forEach((modalClose) => {
+    modalClose.addEventListener("click", (evt) => {
+      const modal = evt.target.closest(".modal");
+      modal.classList.toggle("modal_is-opened");
+    });
+  });
+
+  travelerProfileAddPlaceBtn.addEventListener("click", () => {
+    ModalNewPlace.classList.toggle("modal_is-opened");
+  });
+
+  ModalNewPlace.addEventListener("submit", (evt) => {
+    console.log("Click");
+    const tempCard = {};
+    evt.preventDefault();
+    modalForm = ModalNewPlace.querySelector(".modal__form");
+    const modalInputs = Array.from(modalForm.querySelectorAll(".modal__input"));
+    modalInputs.forEach((modalInput) => {
+      tempCard[modalInput.name] = modalInput.value;
+    });
+
+    createCard(tempCard);
+  });
+
   placeCardDeleteButton.addEventListener("click", (evt) => {
     evt.target.closest(".place-card").remove();
-
-    //pop //push //slice
   });
 
   placeCardLikeButton.addEventListener("click", () => {
@@ -119,4 +150,3 @@ cards.forEach((card) => {
 });
 
 travelerProfileName.textContent = "Gordon Freeman";
-/*<h1 class="traveler-profile__name">Victor Solis</h1>*/
